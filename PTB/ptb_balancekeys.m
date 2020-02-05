0bb5b443c39f1f95e908b3b1e5f1379fadebbde9
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-05 22:32:34 +0400

    new function to counterbalance response keys
    probably this is not useful :(

---
+++ PTB/ptb_balancekeys.m
@@ -0,0 +1,29 @@
+function respKeys = ptb_balancekeys(subjCode, respKeys, method)
+% This function counterbalance the keys based on subjCode.
+%
+% Inputs:
+%     subjCode         <string> subject code
+%     respKeys         <cell of strings> response keys
+%     method           <integer> 1 (for future development)
+%
+% Output:
+%     respKeys         <cell of strings> response keys after counterbalance
+%
+% Created by Haiyang Jin (05-Feb-2020)
+
+if ischar(subjCode)
+    subjCode = str2double(subjCode);
+end
+
+if naring < 3 || isempty(method)
+    method = 1;
+end
+
+switch method
+    case 1 % method 1
+        %
+        if mod(subjCode, 2)
+            respKeys = respKeys(:, 2, 1);
+        end
+        
+end
