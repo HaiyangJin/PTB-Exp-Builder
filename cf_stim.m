853505552ca56f8c99f1a175e3676ab438c7fc13
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-14 14:31:16 +0400

    update comments

--- cf_stim.m
+++ cf_stim.m
@@ -10,18 +10,17 @@ function param = cf_stim(param, stimuli)
 
 screenRect = param.screenRect;
 
+% stimulus dimentions
 [faceY, faceX] = size(stimuli(1,1).matrix);
-
-param.faceAlpha = stimuli(1,1).alpha;
-
 faceCenter = CenterRect([0 0 faceX faceY], screenRect);
-
 param.faceY = faceY;
 param.faceX = faceX;
 
+% stimulus rect information
 param.faceTopRect = [0 0 faceX faceY/2];
 param.faceBottomRect = [0 faceY/2 faceX faceY];
 
+% stimulus positions 
 param.faceTopPosition = [faceCenter(1)...
     faceCenter(2)-1 ...
     faceCenter(3) ...
@@ -32,6 +31,7 @@ param.faceBottomPosition =[faceCenter(1) ...
     faceCenter(3) ...
     faceCenter(4)+2];
 
+% white line information
 misalignSize = param.faceX * param.misalignPerc;
 param.lineRect = [screenRect(3)/2-(3*misalignSize + (faceCenter(3)-faceCenter(1)))/2 ...
     screenRect(4)/2-1 ...
