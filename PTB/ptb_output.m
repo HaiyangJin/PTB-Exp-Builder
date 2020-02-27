929d7b9ca2f878286cd9d6bd56675fa5139a2958
Haiyang <hjin317@aucklanduni.ac.nz>
2020-02-27 15:36:44 +0400

    add one in-argument to add some strings at the end of filename

--- PTB/ptb_output.m
+++ PTB/ptb_output.m
@@ -1,11 +1,18 @@
-function acc = ptb_output(param, stimuli, outputPath)
+function [acc, nResp] = ptb_output(param, stimuli, fnExtra, outputPath)
+% [acc, nResp] = ptb_output(param, stimuli, fnExtra, outputPath)
+%
 % Save the experiment parameters, stimuli structure, and save the output as
 % *.xlsx and *.csv.
 % 
 % Inputs:
-%     param         experiment parameters
-%     stimuli       stimuli structure used in this experiment
-%     output_path   where these files will be saved
+%     param         <structure> experiment parameters
+%     stimuli       <structure> stimuli structure used in this experiment
+%     fnExtra       <string> unique strings at the end of the file name
+%     outputPath    <string> where these files will be saved
+% 
+% Output:
+%     acc           <numeric> the average accuracy across all trials.
+%     nResp         <integer> the number of responses detected.
 %
 % Created by Haiyang Jin (2018)
 
@@ -13,8 +20,12 @@ function acc = ptb_output(param, stimuli, outputPath)
 if isempty(param.dtTable)
     acc = 0;
     return;
+elseif ismember('isCorrect', param.dtTable.Properties.VariableNames)
+    acc = 100*mean(param.dtTable.isCorrect, 'omitnan');
+    nResp = sum(~isnan(param.dtTable.isCorrect));
 else
-    acc = 100*mean(param.dtTable.isCorrect);
+    acc = NaN;
+    nResp = NaN;
 end
 
 % Do not save files if debug model is on
@@ -22,25 +33,24 @@ if param.isDebug
     return;
 end
 
-if nargin < 3 || isempty(outputPath)
+if nargin < 3 || isempty(fnExtra)
+    fnExtra = datestr(now,'yyyy-mm-dd-HHMM');
+end
+
+if nargin < 4 || isempty(outputPath)
     outputPath = pwd;
 end
 
 %% file names
-excelDir = fullfile(outputPath, 'Excel Data');
-saveDir = fullfile(outputPath, 'Matlab Data');
-if ~exist(excelDir, 'dir'); mkdir(excelDir); end
-if ~exist(saveDir, 'dir'); mkdir(saveDir); end
+excelPath = fullfile(outputPath, 'Excel Data');
+matPath = fullfile(outputPath, 'Matlab Data');
+if ~exist(excelPath, 'dir'); mkdir(excelPath); end
+if ~exist(matPath, 'dir'); mkdir(matPath); end
 % backupDir = ['']; % this should be the full path to the dropbox on the main experiment computer
 
-thisDateVector = now;
-theDateString = datestr(thisDateVector,'yyyy-mm-dd-HHMM');
-% theDate8 = str2double(datestr(thisDateVector,'yyyymmdd'));
-
-theDataFn = [param.subjCode '_' param.expCode '_' param.expAbbv '_' theDateString];
-theExcelFile = fullfile(excelDir, [theDataFn '.xlsx']);
-theMatlabFile = fullfile(saveDir, [theDataFn '.mat']);
-
+outputFn = [param.subjCode '_' param.expCode '_' param.expAbbv '_' fnExtra];
+theExcelFile = fullfile(excelPath, [outputFn '.xlsx']);
+theMatlabFile = fullfile(matPath, [outputFn '.mat']);
 
 %% save the files
 save(theMatlabFile, 'param', 'stimuli');
