c36a338388b2329577410023665535aed784cece
Haiyang <hjin317@aucklanduni.ac.nz>
2020-03-03 13:26:04 +0400

    add option to save the output as string instead of cell

--- fMRI/fmri_key2finger.m
+++ fMRI/fmri_key2finger.m
@@ -1,4 +1,4 @@
-function fingers = fmri_key2finger(keys)
+function fingers = fmri_key2finger(keys, saveCell)
 % fingers = fmri_key2finger(keys)
 %
 % This function converts the key names [1to 5] to fingers.
@@ -6,12 +6,20 @@ function fingers = fmri_key2finger(keys)
 % Input:
 %    keys                <numeric> or <cell of strings> KeyNames used for
 %                        responses. It has to be from 1 to 5.
+%    saveCell            <logical> 1: the output will be saved as a cell.
+%                        0: the output will be saved as a string if only
+%                        one finger name is obtained.
 %
 % Output:
 %    fingers             <cell of strings> names of fingers to be used in
 %                        fMRI experiments (the response box)
 %
 % Created by Haiyang Jin (3-March-2020)
+
+% By default fingers will be a string if there is only one finger in the output 
+if nargin < 2 || isempty(saveCell)
+    saveCell = 0;
+end
 
 % keys to be allowed in fMRI
 keyAllow = 1:5;
@@ -37,5 +45,10 @@ end
 
 % get the finger names for the keys
 fingers = fingerNames(keys);
+
+% conver the fingers to a string if needed
+if numel(fingers) == 1 && ~saveCell
+    fingers = fingers{1};
+end
 
 end
