629ffe0ccfdd81cc9fadef7272fe007c3343bd83
Haiyang <hjin317@aucklanduni.ac.nz>
2020-09-28 20:48:01 +0400

    add more options to create misaligned composite stimuli

--- ImageTools/im_mkcomposite.m
+++ ImageTools/im_mkcomposite.m
@@ -1,4 +1,4 @@
-function compStimDir = im_mkcomposite(stimDir, gapPixels, gapColor)
+function compStimDir = im_mkcomposite(stimDir, misalignPx, gapPx, gapExtra, gapColor)
 % compStimDir = im_mkcomposite(stimDir, gapPixels, gapColor)
 %
 % Create composite stimuli by combining the top and bottom halves of
@@ -7,7 +7,11 @@ function compStimDir = im_mkcomposite(stimDir, gapPixels, gapColor)
 % Inputs:
 %    stimDir      <structure> stimulus structure created by im_dir and
 %                  im_readdir.
-%    gapPixels    <integer> pixels used for the gap between top and bottom
+%    misalignedPx <integer> the size (in pixels) of misaligned. Positive
+%                  number refers to misalignment to right; negative is to
+%                  the left. Default is the half of the stim width to the
+%                  right (and the aligned images).
+%    gapPx        <integer> pixels used for the gap between top and bottom
 %                  halves. Default is 3.
 %    gapColor     <integer vector> 1X3 vector. Default is [255, 255, 255],
 %                  i.e., white.
@@ -24,9 +28,16 @@ function compStimDir = im_mkcomposite(stimDir, gapPixels, gapColor)
 % compStimDir = im_mkcomposite(stimDir);
 % im_writedir(compStimDir);
 
-if ~exist('gapPixels', 'var') || isempty(gapPixels)
-    gapPixels = 3;
+if ~exist('misalignPx', 'var') || isempty(misalignPx)
+    misalignPx = size(stimDir(1).matrix, 2)/2;
 end
+
+if ~exist('gapPx', 'var') || isempty(gapPx)
+    gapPx = 3;
+end
+
+if ~exist('gapExtra', 'var') || isempty(gapExtra)
+    gapExtra = size(stimDir(1).matrix, 2)*.25;
 
 if ~exist('gapColor', 'var') || isempty(gapColor)
     gapColor = [255, 255, 255];
@@ -36,6 +47,9 @@ end
 groups = {stimDir.condition};
 groupList = unique(groups);
 nGroup = numel(groupList);
+
+misalignment = [0, misalignPx];
+nAlign = numel(misalignment);
 
 groupStimCell = cell(nGroup, 1);
 
@@ -52,7 +66,7 @@ for iGroup = 1:nGroup
     combinations = [halfComb; halfComb(:, [2, 1])];
     
     nComb = size(combinations, 1);
-    tempGroupCell = cell(nComb, 1);
+    tempGroupCell = cell(nComb, nAlign);
     
     for iComb = 1:nComb
         
@@ -63,38 +77,61 @@ for iGroup = 1:nGroup
         
         % save the corresponding fields
         temp = struct;
-        temp.fn = sprintf('%s_%s%s', tempFn1, tempFn2, ext);
-%         temp.foler = fullfile(outPath, thisCond);
+        %         temp.foler = fullfile(outPath, thisCond);
         temp.condition = thisGroup;
         
         % save the matrix for composite stimuli
         [stimX, stimY, nLayer] = size(groupDir(thisComb(1)).matrix);
         halfXIndex = floor(stimX/2);
         
-        composite_matrix = NaN(stimX+gapPixels, stimY, nLayer);
-        % create composite stimuli for each layer separately
-        for iLayer = 1:nLayer
-            composite_matrix(:, :, iLayer) = vertcat(groupDir(thisComb(1)).matrix(1:halfXIndex, :, iLayer), ...
-                ones(gapPixels, stimY) * gapColor(iLayer), ...
-                groupDir(thisComb(2)).matrix(halfXIndex+1 : stimX, :, iLayer));
-        end
-        temp.matrix = composite_matrix;
+        % initialize a image matrix for one layer
+        compMatrix = ones(halfXIndex, stimY+misalignPx*2+gapExtra*2)*255;
         
-        % save alpha if it is available
-        if isfield(groupDir(thisComb(1)), 'alpha')
-            temp.alpha = vertcat(groupDir(thisComb(1)).alpha(1:halfXIndex, :), ...
-                ones(gapPixels, stimY) * 255, ...
-                groupDir(thisComb(2)).alpha(halfXIndex+1 : stimX, :));
+        for iAli = 1:nAlign
+                       
+            % misalign size in pixel for this image
+            misalign = misalignment(iAli);
+            
+            if misalign == 0; misStr = 'ali'; else; misStr = 'mis'; end
+            temp.fn = sprintf('%s_%s_%s%s', tempFn1, tempFn2, misStr, ext);
+            
+            composite_matrix = NaN(stimX+gapPx, stimY+misalignPx*2+gapExtra*2, nLayer);
+            
+            % create composite stimuli for each layer separately
+            for iLayer = 1:nLayer
+                topHalf = compMatrix;
+                bottomHalf = compMatrix;
+                
+                topHalf(:, misalignPx+gapExtra+(1:stimY)) = groupDir(thisComb(1)).matrix(1:halfXIndex, :, iLayer);
+                gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * gapColor(iLayer);
+                bottomHalf(:, misalignPx+gapExtra+misalign+(1:stimY)) = groupDir(thisComb(2)).matrix(halfXIndex+1 : stimX, :, iLayer);
+                
+                composite_matrix(:, :, iLayer) = vertcat(topHalf, gapLine, bottomHalf);
+            end
+            temp.matrix = composite_matrix;
+            
+            % save alpha if it is available
+            if isfield(groupDir(thisComb(1)), 'alpha')
+                topHalf = compMatrix*0;
+                bottomHalf = compMatrix*0;
+                
+                topHalf(:, misalignPx+gapExtra+(1:stimY)) = groupDir(thisComb(1)).alpha(1:halfXIndex, :);
+                gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * 255;
+                bottomHalf(:, misalignPx+gapExtra+misalign+(1:stimY)) = groupDir(thisComb(2)).alpha(halfXIndex+1 : stimX, :);
+                
+                temp.alpha = vertcat(topHalf, gapLine, bottomHalf);
+            end
+            
+            % save the structure for this combination/composite stimulus
+            tempGroupCell{iComb, iAli} = temp;
+            
         end
-        
-        % save the structure for this combination/composite stimulus
-        tempGroupCell{iComb, 1} = temp;
         
     end
     
     % save structure for this group
-    groupStimCell{iGroup, 1} = vertcat(tempGroupCell{:}); 
-
+    groupStimCell{iGroup, 1} = vertcat(tempGroupCell{:});
+    
 end
 
 % save the stim dir for all composite stimuli
