3d22976793f3d82830e17ff8cffe2b7270c4404d
Haiyang <hjin317@aucklanduni.ac.nz>
2020-12-13 15:31:51 +0400

    use varargin
    add 'cued half' and 'group fieldname'

--- ImageTools/im_mkcomposite.m
+++ ImageTools/im_mkcomposite.m
@@ -1,5 +1,5 @@
-function compStimDir = im_mkcomposite(stimDir, groupFn, misalignPx, gapPx, gapExtra, gapColor)
-% compStimDir = im_mkcomposite(stimDir, gapPixels, gapColor)
+function compStimDir = im_mkcomposite(stimDir, varargin)
+% compStimDir = im_mkcomposite(stimDir, ...)
 %
 % Create composite stimuli by combining the top and bottom halves of
 % stimuli in the same group.
@@ -7,17 +7,26 @@ function compStimDir = im_mkcomposite(stimDir, groupFn, misalignPx, gapPx, gapEx
 % Inputs:
 %    stimDir      <structure> stimulus structure created by im_dir and
 %                  im_readdir.
-%    groupFn      <string> group fieldname to divide the faces into groups.
+%
+% Varargin:
+%    groupfn      <string> group fieldname to divide the faces into groups.
 %                  Default is '' (i.e., only one group for all faces).
-%    misalignedPx <integer> the size (in pixels) of misaligned. Positive
+%    misratio     <numeric> the size (in pixels) of misaligned. Positive
 %                  number refers to misalignment to right; negative is to
-%                  the left. Default is the half of the stim width to the
-%                  right (and the aligned images).
-%    gapPx        <integer> pixels used for the gap between top and bottom
+%                  the left. If misratio is from -1 to 1, the misalignment
+%                  will be stim width times misratio. If the absolute value
+%                  of misratio is larger than 1, the misalignment will be
+%                  misratio. Default is 0.5, i.e., the half of the stim
+%                  width to the right (and the aligned images).
+%    cuedhalf     <integer> which half is the cued half; the cued half will
+%                  remain in the center of image. 1: top; 2: bottom; 3:
+%                  both 1 and 2. Default is 1.
+%    gappx        <integer> pixels used for the gap between top and bottom
 %                  halves. Default is 3.
-%    gapExtra     <numeric> the width of the gap line. Default is one
-%                  quarter of the face width (for each side).
-%    gapColor     <integer vector> 1X3 vector. Default is [255, 255, 255],
+%    gapextra     <numeric> the width of the gap line. gapextra can only be
+%                  postive. Its usage is similar to misratio. Default is
+%                  0.25, i.e., one quarter of the face width (for each side).
+%    gapcolor     <integer vector> 1X3 vector. Default is [255, 255, 255],
 %                  i.e., white.
 %
 % Output:
@@ -32,23 +41,33 @@ function compStimDir = im_mkcomposite(stimDir, groupFn, misalignPx, gapPx, gapEx
 % compStimDir = im_mkcomposite(stimDir);
 % im_writedir(compStimDir);
 
-if ~exist('groupFn', 'var') || isempty(groupFn)
-    groupFn = '';
+defaultOpts = struct();
+defaultOpts.groupfn = '';
+defaultOpts.misratio = 0.5;
+defaultOpts.cuedhalf = 1;
+defaultOpts.gappx = 3;
+defaultOpts.gapextra = 0.25;
+defaultOpts.gapcolor = [255, 255, 255];
+
+opts = ptb_mergestruct(defaultOpts, varargin{:});
+
+stimWidth = size(stimDir(1).matrix, 2);
+groupFn = opts.groupfn;
+if abs(opts.misratio) < 1
+    misalignPx = stimWidth * opts.misratio;
+else
+    misalignPx = opts.misratio;
 end
-
-if ~exist('misalignPx', 'var') || isempty(misalignPx)
-    misalignPx = size(stimDir(1).matrix, 2)/2;
+if opts.cuedhalf == 3
+    cuedHalf = [1, 2];
+else
+    cuedHalf = opts.cuedhalf;
 end
-
-if ~exist('gapPx', 'var') || isempty(gapPx)
-    gapPx = 3;
-end
-
-if ~exist('gapExtra', 'var') || isempty(gapExtra)
-    gapExtra = size(stimDir(1).matrix, 2)*.25;
-
-if ~exist('gapColor', 'var') || isempty(gapColor)
-    gapColor = [255, 255, 255];
+gapPx = opts.gappx;
+if abs(opts.gapextra) < 1
+    gapExtra = stimWidth * opts.gapextra;
+else
+    gapExtra = opts.gapextra;
 end
 
 % group information
@@ -66,6 +85,8 @@ nAlign = numel(misalignment);
 
 groupStimCell = cell(nGroup, 1);
 
+cueStr = {'top', 'bot'};
+
 % Create composite stimuli within each group
 for iGroup = 1:nGroup
     
@@ -79,7 +100,7 @@ for iGroup = 1:nGroup
     combinations = [halfComb; halfComb(:, [2, 1])];
     
     nComb = size(combinations, 1);
-    tempGroupCell = cell(nComb, nAlign);
+    tempGroupCell = cell(nComb, nAlign, numel(cuedHalf));
     
     for iComb = 1:nComb
         
@@ -101,42 +122,50 @@ for iGroup = 1:nGroup
         compMatrix = ones(halfXIndex, stimY+misalignPx*2+gapExtra*2)*255;
         
         for iAli = 1:nAlign
-                       
+            
             % misalign size in pixel for this image
             misalign = misalignment(iAli);
             
             if misalign == 0; misStr = 'ali'; else; misStr = 'mis'; end
-            temp.fn = sprintf('%s_%s_%s%s', tempFn1, tempFn2, misStr, ext);
-            
+                        
             composite_matrix = NaN(stimX+gapPx, stimY+misalignPx*2+gapExtra*2, nLayer);
             
-            % create composite stimuli for each layer separately
-            for iLayer = 1:nLayer
-                topHalf = compMatrix;
-                bottomHalf = compMatrix;
+            for iCue = cuedHalf
                 
-                topHalf(:, misalignPx+gapExtra+(1:stimY)) = groupDir(thisComb(1)).matrix(1:halfXIndex, :, iLayer);
-                gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * gapColor(iLayer);
-                bottomHalf(:, misalignPx+gapExtra+misalign+(1:stimY)) = groupDir(thisComb(2)).matrix(halfXIndex+1 : stimX, :, iLayer);
+                temp.fn = sprintf('%s_%s_%s_%s%s', cueStr{iCue}, tempFn1, tempFn2, misStr, ext);
                 
-                composite_matrix(:, :, iLayer) = vertcat(topHalf, gapLine, bottomHalf);
+                topPosi = misalignPx+gapExtra+(1:stimY) + misalign * (iCue-1);
+                bottomPosi = misalignPx+gapExtra+(1:stimY) + misalign * (2-iCue);
+                
+                % create composite stimuli for each layer separately
+                for iLayer = 1:nLayer
+                    topHalf = compMatrix;
+                    bottomHalf = compMatrix;
+                    
+                    topHalf(:, topPosi) = groupDir(thisComb(1)).matrix(1:halfXIndex, :, iLayer);
+                    gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * opts.gapcolor(iLayer);
+                    bottomHalf(:, bottomPosi) = groupDir(thisComb(2)).matrix(halfXIndex+1 : stimX, :, iLayer);
+                    
+                    composite_matrix(:, :, iLayer) = vertcat(topHalf, gapLine, bottomHalf);
+                end
+                temp.matrix = composite_matrix;
+                
+                % save alpha if it is available
+                if isfield(groupDir(thisComb(1)), 'alpha')
+                    topHalf = compMatrix*0;
+                    bottomHalf = compMatrix*0;
+                    
+                    topHalf(:,topPosi) = groupDir(thisComb(1)).alpha(1:halfXIndex, :);
+                    gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * 255;
+                    bottomHalf(:, bottomPosi) = groupDir(thisComb(2)).alpha(halfXIndex+1 : stimX, :);
+                    
+                    temp.alpha = vertcat(topHalf, gapLine, bottomHalf);
+                end
+
+                % save the structure for this combination/composite stimulus
+                tempGroupCell{iComb, iAli, iCue} = temp;
+                
             end
-            temp.matrix = composite_matrix;
-            
-            % save alpha if it is available
-            if isfield(groupDir(thisComb(1)), 'alpha')
-                topHalf = compMatrix*0;
-                bottomHalf = compMatrix*0;
-                
-                topHalf(:, misalignPx+gapExtra+(1:stimY)) = groupDir(thisComb(1)).alpha(1:halfXIndex, :);
-                gapLine = ones(gapPx, stimY+misalignPx*2+gapExtra*2) * 255;
-                bottomHalf(:, misalignPx+gapExtra+misalign+(1:stimY)) = groupDir(thisComb(2)).alpha(halfXIndex+1 : stimX, :);
-                
-                temp.alpha = vertcat(topHalf, gapLine, bottomHalf);
-            end
-            
-            % save the structure for this combination/composite stimulus
-            tempGroupCell{iComb, iAli} = temp;
             
         end
         
