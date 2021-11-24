f93abb28ff1fd310078f8f2d017e0e7da7214925
Haiyang <hjin317@aucklanduni.ac.nz>
2021-11-24 09:54:54 +0400

    use ptb_addpath directly

--- ptb_addpath.m
+++ ptb_addpath.m
@@ -1,23 +1,23 @@
-function pathlist = ptb_addpath(ptbpath, folderlist, remove)
-% pathlist = ptb_addpath(ptbpath, folderlist, remove)
+function pathlist = ptb_addpath(add, ptbpath, folderlist)
+% pathlist = ptb_addpath(add, ptbpath, folderlist)
 %
 % Adds functions to Matlab Path (useful when developing new programs). This
-% function is not recommended to be used in real experiments. 
-% 1. Functions in 'PTB-Exp-Builder/' will be added by default. 
-% 2. If PTB-Exp-Builder/' is not avaiable in Matlab Path, 'functions/' in 
+% function is not recommended to be used in real experiments.
+% 1. Functions in 'PTB-Exp-Builder/' will be added by default.
+% 2. If PTB-Exp-Builder/' is not avaiable in Matlab Path, 'functions/' in
 %    pwd (if available) will be added to Matlab Path.
 % 3. If 'functions/' is not avaiable in pwd, <folderlist> in the pwd will
-%    be added to Matlab Path. 
+%    be added to Matlab Path.
 %
 % Inputs:
+%    add            <boo> whether add <pathlist> from Matlab Path.
+%                    Default is 1.
 %    ptbpath        <str> where folders in <folderlist> are.
-%    folderlist     <cell str> folders to be added to Matlab Path. 
-%    remove         <boo> whether remove <pathlist> from Matlab Path.
-%                    Default is 0 (i.e., add).
+%    folderlist     <cell str> folders to be added to Matlab Path.
 %
 % Output:
 %    pathlist       <cell str> list of folders added to /removed from
-%                    Matlab path. 
+%                    Matlab path.
 %
 % Created by Haiyang Jin (2021-11-22)
 
@@ -30,24 +30,29 @@ end
 
 if ~exist('ptbpath', 'var') || isempty(ptbpath)
     % use functions in PTB-Exp-Builder by default
-    ptbpath = fileparts(which('ptb_addminipath'));
+    ptbpath = fileparts(which('ptb_addpath'));
 end
 if isempty(ptbpath)
     % if PTB-Exp-Builder is not added to path, use 'functions/' in pwd.
     ptbpath = 'functions/';
 end
 
-if ~exist('remove', 'var') || isempty(remove)
-    remove = 0;
+if ~exist('add', 'var') || isempty(add)
+    add = 1;
 end
 
 % add all avaiable folders to Matlab path
-pathlist = fullfile(ptbpath, folderlist);
+pathlist = fullfile(ptbpath, folderlist)';
 
-if ~remove
+if add
     cellfun(@addpath, pathlist);
 else
     cellfun(@rmpath, pathlist);
 end
+
+% display message
+dirstr = {'removed from', 'added to'};
+fprintf('\nFollowing directories have been %s Matlab path:\n%s', ...
+    dirstr{add+1}, sprintf('%s\n', pathlist{:}));
 
 end
