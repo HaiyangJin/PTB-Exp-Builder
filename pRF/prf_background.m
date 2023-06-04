e857eb5c85b2935b87694a606ce88232ae693419
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-05 00:04:57 +0400

    fix bug

--- pRF/prf_background.m
+++ pRF/prf_background.m
@@ -6,7 +6,7 @@ function prf_background(param)
 % Created by Haiyang Jin (2023-May-1)
 
 % draw line array
-ptb_bgarray(param);
+prf_bgarray(param);
 % draw dot array
 Screen('DrawDots', param.w, param.prfposi2, param.dotva, param.dcolor, ...
     [param.screenCenX, param.screenCenY], 1);
