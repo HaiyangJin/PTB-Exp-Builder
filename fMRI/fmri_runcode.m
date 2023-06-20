4d3687622db826f2b7d1b22389c45b78cca3387d
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2023-06-20 20:37:02 +0400

    identify run number/code

--- fMRI/fmri_runcode.m
+++ fMRI/fmri_runcode.m
@@ -1,5 +1,5 @@
-function runCode = fmri_runcode(param, outPath)
-% runCode = frmi_runcode(param, outPath)
+function runCode = fmri_runcode(param, extrawc, outPath)
+% runCode = frmi_runcode(param, extrawc, outPath)
 %
 % This function generates the run code based on how many similar output
 % files are in the Matlab Data/ folder.
@@ -7,6 +7,7 @@ function runCode = fmri_runcode(param, outPath)
 % Inputs:
 %     param         <struct> the experiment parameters. [only use .SubjCode,
 %                    .expCode, .expAbbv]
+%     extrawc       <str> additional wildcard to identify run information.
 %     outPath       <str> the path to save the output files.
 %
 % Output:
@@ -14,16 +15,17 @@ function runCode = fmri_runcode(param, outPath)
 % 
 % Created by Haiyang Jin (27-Feb-2020)
 
-if ~exist('outPath', 'var') || isempty(outPath)
-    outPath = pwd;
+if ~exist('extrawc', 'var') || isempty(extrawc)
+    extrawc = '';
 end
 
-if isfield(param, 'outpath')
-    outPath = param.outpath;
+if ~exist('outPath', 'var') || isempty(outPath)
+    outPath = '';
+    if isfield(param, 'outpath'); outPath = param.outpath; end
 end
 
 % outputfilename
-outputFn = sprintf('%s_%s_%s_Run*', param.subjCode, param.expCode, param.expAbbv);
+outputFn = sprintf('%s_%s_%s_Run*%s', param.subjCode, param.expCode, param.expAbbv, extrawc);
 
 % dir the similar output files
 matDir = dir(fullfile(outPath, 'MatBackup', outputFn));
